41cad086ef5fe52c01b5a8c98c520ca62168b3d7
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-05 23:34:41 +0200

    implement vector arithmetic

---
+++ src/math/Vec2_arithmetic.cpp
@@ -0,0 +1,50 @@
+#include "Vec2.hpp"
+
+Vec2 operator+(Vec2 lhs, Vec2 rhs) {
+    return {lhs.x + rhs.x, lhs.y + rhs.y};
+}
+
+Vec2 operator-(Vec2 v) {
+    return {-v.x, -v.y};
+}
+
+Vec2 operator-(Vec2 lhs, Vec2 rhs) {
+    return lhs + (-rhs);
+}
+
+Vec2 operator*(Vec2::Scalar s, Vec2 rhs) {
+    return {s * rhs.x, s * rhs.y};
+}
+
+Vec2 operator/(Vec2 lhs, Vec2::Scalar s) {
+    return (1.0/s) * lhs;
+}
+
+Vec2 Vec2::operator+=(Vec2 other) {
+    *this = *this + other;
+    return *this;
+}
+
+Vec2 Vec2::operator-=(Vec2 other) {
+    *this = *this - other;
+    return *this;
+}
+
+Vec2 Vec2::operator*=(Scalar s) {
+    *this = s * *this;
+    return *this;
+}
+
+Vec2 Vec2::operator/=(Scalar s) {
+    *this = *this / s;
+    return *this;
+}
+
+
+bool Vec2::operator==(Vec2 other) const {
+    return (this->x == other.x) && (this->y == other.y);
+}
+
+bool Vec2::operator!=(Vec2 other) const {
+    return !(*this == other);
+}
