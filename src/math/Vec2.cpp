b0f7916c18145cb93e2d7967d96fc9a3897cfade
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-05 23:39:14 +0200

    implement vector operations

--- src/math/Vec2.cpp
+++ src/math/Vec2.cpp
@@ -2,6 +2,30 @@
 
 #include <sstream>
 
+Vec2::Scalar Vec2::magnitude() const {
+    return this->dot(*this);
+}
+
+void Vec2::normalize() {
+    *this /= this->magnitude();
+}
+
+Vec2::Scalar Vec2::dot(Vec2 other) const {
+    return Vec2::dot(*this, other);
+}
+
+Vec2::Scalar Vec2::dot(Vec2 lhs, Vec2 rhs) {
+    return lhs.x * rhs.x + lhs.y + rhs.y;
+}
+
+Vec2 Vec2::cross(Vec2 other) const {
+    return Vec2::cross(*this, other);
+}
+
+Vec2 Vec2::cross(Vec2 lhs, Vec2 rhs) {
+    return {lhs.x * rhs.y - lhs.y * rhs.x, lhs.y * rhs.x - lhs.x * rhs.y};
+}
+
 std::string Vec2::repr() const {
     std::stringstream s;
 
