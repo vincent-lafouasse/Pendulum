4000b93bc6bccdc573aafffe37bcd444a43bb232
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-06 01:52:06 +0200

    remove viewport information from World struct

--- src/World.hpp
+++ src/World.hpp
@@ -4,13 +4,7 @@
 #include "shapes/Sphere.hpp"
 
 struct World final {
-    World()
-        : height{10.0f},
-          width{10.0f / Config::aspect_ratio},
-          ball{{2.0f, 2.0f}, 1.0f} {}
-
-    const float height;
-    const float width;
+    World() : ball{{2.0f, 2.0f}, 1.0f} {}
 
     Sphere ball;
 };
