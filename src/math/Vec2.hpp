7bb5afe57d3fb7f118036f0025512e56c5992a7e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-05 23:26:35 +0200

    sketch vector operation intercace

--- src/math/Vec2.hpp
+++ src/math/Vec2.hpp
@@ -5,6 +5,15 @@
 struct Vec2 final {
     using Scalar = float;
 
+    Scalar magnitude() const;
+    void normalize();
+
+    Scalar dot(Vec2 other) const;
+    static Scalar dot (Vec2 lhs, Vec2 rhs);
+
+    Vec2 cross(Vec2 other) const;
+    static Vec2 cross(Vec2 lhs, Vec2 rhs);
+
     Vec2 operator+=(Vec2 other);
     Vec2 operator-=(Vec2 other);
     Vec2 operator*=(Scalar s);
