fdc6ee0ac7912466b61bae1f55716381dffcc2d5
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-05 23:24:17 +0200

    sketch vec2 arithmetics

--- src/math/Vec2.hpp
+++ src/math/Vec2.hpp
@@ -3,8 +3,24 @@
 #include <string>
 
 struct Vec2 final {
+    using Scalar = float;
+
+    Vec2 operator+=(Vec2 other);
+    Vec2 operator-=(Vec2 other);
+    Vec2 operator*=(Scalar s);
+    Vec2 operator/=(Scalar s);
+
+    bool operator==(Vec2 other) const; 
+    bool operator!=(Vec2 other) const; 
+
     std::string repr() const;
 
-    float x;
-    float y;
+    Scalar x;
+    Scalar y;
 };
+
+Vec2 operator+(Vec2 lhs, Vec2 rhs);
+Vec2 operator-(Vec2 lhs, Vec2 rhs);
+
+Vec2 operator*(Vec2::Scalar s, Vec2 rhs);
+Vec2 operator/(Vec2 lhs, Vec2::Scalar s);
