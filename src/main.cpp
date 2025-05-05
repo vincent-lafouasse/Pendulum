603bc061f1413f73f2ab82015067a2dc94228bae
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-05 16:57:58 +0200

    wrap static constexpr globals in a Config struct

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <SDL2/SDL.h>
 
-constexpr float aspect_ratio = 16.0f / 9.0f;
-constexpr size_t width = 800;
-constexpr size_t height = static_cast<size_t>(width / aspect_ratio);
-constexpr float target_fps = 10;
+struct Config final {
+    static constexpr float aspect_ratio = 16.0f / 9.0f;
+    static constexpr size_t width = 800;
+    static constexpr size_t height = static_cast<size_t>(width / aspect_ratio);
+    static constexpr float target_fps = 10.0f;
+    static constexpr size_t ms_per_frame =
+        static_cast<size_t>(1000.0f / target_fps);
+};
 
 #define NICE_BLUE 33, 118, 174, 255
 
-void cap_fps(uint32_t frame_beginning_tick, float target_fps);
+void cap_fps(uint32_t frame_beginning_tick);
 void init_SDL(SDL_Window** return_window, SDL_Renderer** return_renderer);
 
 int main() {
@@ -30,18 +34,17 @@ int main() {
 
         SDL_RenderPresent(renderer);
 
-        cap_fps(frame_beginning_tick, target_fps);
+        cap_fps(frame_beginning_tick);
     }
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return (EXIT_SUCCESS);
 }
-void cap_fps(uint32_t frame_beginning_tick, float target_fps) {
-    const float ms_per_frame = 1000.0f / target_fps;
+void cap_fps(uint32_t frame_beginning_tick) {
     const uint32_t frame_end_tick = SDL_GetTicks();
-    const uint32_t time_to_wait = static_cast<uint32_t>(ms_per_frame) -
-                                  (frame_end_tick - frame_beginning_tick);
+    const uint32_t time_to_wait =
+        Config::ms_per_frame - (frame_end_tick - frame_beginning_tick);
 
     if (time_to_wait > 0) {
         SDL_Delay(time_to_wait);
@@ -56,8 +59,9 @@ void init_SDL(SDL_Window** return_window, SDL_Renderer** return_renderer) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         exit(EXIT_FAILURE);
     }
-    *return_window = SDL_CreateWindow("a window", SCREEN_X_POS, SCREEN_Y_POS,
-                                      width, height, SDL_WINDOW_OPENGL);
+    *return_window =
+        SDL_CreateWindow("a window", SCREEN_X_POS, SCREEN_Y_POS, Config::width,
+                         Config::height, SDL_WINDOW_OPENGL);
     if (*return_window == nullptr) {
         SDL_Log("Could not create a window: %s", SDL_GetError());
         exit(EXIT_FAILURE);
