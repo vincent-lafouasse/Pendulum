d945cbbd2d3551a8c8047355d53668ec30832bc0
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-06 01:54:59 +0200

    implement Sphere.contains(Point2)

--- src/shapes/Sphere.hpp
+++ src/shapes/Sphere.hpp
@@ -3,6 +3,10 @@
 #include "math/Vec2.hpp"
 
 struct Sphere final {
+    bool contains(Point2 point) const {
+        return (point - center).magnitude() <= radius * radius;
+    }
+
     Point2 center;
     float radius;
 };
