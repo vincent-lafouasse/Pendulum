b4711252fc3911867a8d124668d592520bfbe5bd
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-05 22:27:54 +0200

    throw from Timer.cap_frame() is frame is not init

--- src/Timer.hpp
+++ src/Timer.hpp
@@ -2,17 +2,26 @@
 
 #include <SDL_timer.h>
 
+#include <stdexcept>
+
 #include "Config.hpp"
 
 class Timer final {
    public:
     using UInt = decltype(SDL_GetTicks());
 
-    Timer() : ms_per_frame(Config::ms_per_frame) {}
+    Timer() : ms_per_frame(Config::ms_per_frame), frame_start_is_init(false) {}
 
-    void start_frame() { this->frame_start_ms = SDL_GetTicks(); }
+    void start_frame() {
+        this->frame_start_ms = SDL_GetTicks();
+        this->frame_start_is_init = true;
+    }
 
-    void cap_frame() const {
+    void cap_frame() {
+        if (!this->frame_start_is_init) {
+            throw UninitializedFrame();
+        }
+
         const UInt frame_end_ms = SDL_GetTicks();
         const UInt time_to_wait =
             this->ms_per_frame - (frame_end_ms - this->frame_start_ms);
@@ -20,9 +29,17 @@ class Timer final {
         if (time_to_wait > 0) {
             SDL_Delay(time_to_wait);
         }
+        this->frame_start_is_init = false;
     }
+
+    class UninitializedFrame : public std::runtime_error {
+       public:
+        UninitializedFrame()
+            : std::runtime_error("Attempting to cap uninitialized frame") {}
+    };
 
    private:
     const UInt ms_per_frame;
     UInt frame_start_ms;
+    bool frame_start_is_init;
 };
