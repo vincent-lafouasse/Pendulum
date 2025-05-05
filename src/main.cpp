f970f58fe9ae6b1465d7ebf4b42dc0cb0b017b23
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-05 22:37:21 +0200

    init world

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,29 @@
 #include "RenderingCtx.hpp"
 #include "Timer.hpp"
 
+struct Sphere final {
+    float center_x;
+    float center_y;
+    float radius;
+};
+
+struct World final {
+    World()
+        : width{10.0f},
+          height{10.0f / Config::aspect_ratio},
+          ball{2.0f, 2.0f, 1.0f} {}
+
+    const float width;
+    const float height;
+
+    Sphere ball;
+};
+
 int main() {
     RenderingCtx ctx{};
     Timer timer{};
+
+    World world{};
 
     SDL_Event event;
     while (true) {
