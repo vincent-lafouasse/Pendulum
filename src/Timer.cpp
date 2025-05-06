7c614af65ff930768d3b2d93a5fec11e500040b3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-06 02:18:08 +0200

    fix fps capping

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -2,6 +2,8 @@
 
 #include "Config.hpp"
 
+#include <iostream>
+
 Timer::Timer()
     : ms_per_frame(Config::ms_per_frame), frame_start_is_init(false) {}
 
@@ -15,14 +17,16 @@ void Timer::cap_frame() {
         throw UninitializedFrame();
     }
 
-    const UInt frame_end_ms = SDL_GetTicks();
-    const UInt time_to_wait =
-        this->ms_per_frame - (frame_end_ms - this->frame_start_ms);
+    const UInt frame_len_ms = SDL_GetTicks() - this->frame_start_ms;
 
-    if (time_to_wait > 0) {
-        SDL_Delay(time_to_wait);
+    if (frame_len_ms < ms_per_frame) {
+        SDL_Delay(ms_per_frame - frame_len_ms);
     }
     this->frame_start_is_init = false;
+
+    const float actual_frame_len =
+        static_cast<float>(SDL_GetTicks() - this->frame_start_ms) / 1000.0f;
+    std::clog << "fps: " << static_cast<int>(1.0f / actual_frame_len) << '\n';
 }
 
 Timer::UninitializedFrame::UninitializedFrame()
