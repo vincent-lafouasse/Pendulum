5f37f2bafc6db4b9bf212e8d584065f1d90429f3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-06 02:32:44 +0200

    set width and height to be an int as its what the sdl wants

--- src/Config.hpp
+++ src/Config.hpp
@@ -4,16 +4,8 @@
 
 namespace Config {
 constexpr float aspect_ratio = 16.0f / 9.0f;
-constexpr size_t width = 800;
-constexpr size_t height = static_cast<size_t>(width / aspect_ratio);
-constexpr float actual_aspect_ratio =
-    static_cast<float>(width) / static_cast<float>(height);
-
-constexpr float viewport_height = 10.0f;
-constexpr float viewport_width = viewport_height * actual_aspect_ratio;
-
-constexpr float delta_x = viewport_width / width;
-constexpr float delta_y = viewport_height / height;
+constexpr int width = 800;
+constexpr int height = static_cast<int>(width / aspect_ratio);
 
 constexpr float target_fps = 10.0f;
 constexpr size_t ms_per_frame = static_cast<size_t>(1000.0f / target_fps);
