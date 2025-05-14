acfdb5c777d45d35c677ae9a0e738b4f0a6831b0
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-14 23:28:05 +0200

    sketch

--- src/math/Vec2Polar.cpp
+++ src/math/Vec2Polar.cpp
@@ -1,19 +1,17 @@
 #include "Vec2.hpp"
 #include "math/math.hpp"
 
-Vec2 Vec2::as_polar(Vec2 origin) const {
-    Vec2 to_point = *this - origin;
-    Scalar radius = to_point.magnitude();
+Vec2 Vec2::as_polar() const {
+    Scalar radius = this->magnitude();
 
     if (float_equal(radius, 0.0f)) {
         return {0.0f, 0.0f};
     }
 
-    to_point.normalize();
     return {};
 }
 
-Vec2 Vec2::as_cartesian(Vec2 origin) {
+Vec2 Vec2::as_cartesian() const {
     return {};
 }
 
