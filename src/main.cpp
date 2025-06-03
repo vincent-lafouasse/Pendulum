a9aed62d4b2b262dc96a9ac9077e7f384ed800d3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-03 10:54:23 +0200

    format

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main() {
     InitWindow(width, height, "hi");
     SetTargetFPS(60);
 
-    constexpr Vector2 center {width / 2.0f, height / 2.0f};
+    constexpr Vector2 center{width / 2.0f, height / 2.0f};
     constexpr float offset = 100.0f;
     float angle = 0.0f;
 
@@ -18,7 +18,8 @@ int main() {
         ClearBackground(VIOLET);
         {
             BeginDrawing();
-            const Vector2 offsetVector = {offset * std::sin(angle), offset * std::cos(angle)};
+            const Vector2 offsetVector = {offset * std::sin(angle),
+                                          offset * std::cos(angle)};
             const Vector2 circleCenter = Vector2Add(center, offsetVector);
             DrawCircle(circleCenter.x, circleCenter.y, 50.0f, RED);
             const float angularVelocity = std::sin(GetTime());
