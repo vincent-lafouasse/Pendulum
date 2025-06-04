b318fde8a6eed2e17d635e46ce164fd37f71f249
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-04 02:55:05 +0200

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,31 @@
 #include <raylib.h>
 #include <raymath.h>
 
-static constexpr int width = 800;
-static constexpr int height = 600;
-static constexpr Color darkGray {24, 24, 37, 255};
-static constexpr Color lavender {180, 190, 254, 255};
-static constexpr Color blue {137, 180, 250, 255};
+static constexpr int width = 1600;
+static constexpr int height = 900;
+static constexpr Color darkGray{24, 24, 37, 255};
+static constexpr Color lavender{180, 190, 254, 255};
+static constexpr Color blue{137, 180, 250, 255};
 
 int main() {
     InitWindow(width, height, "hi");
     SetTargetFPS(60);
 
     constexpr Vector2 center{width / 2.0f, height / 2.0f};
-    constexpr float offset = 100.0f;
+    constexpr float armLength = 100.0f;
     float angle = 0.0f;
 
     while (!WindowShouldClose()) {
         ClearBackground(darkGray);
         {
             BeginDrawing();
-            const Vector2 offsetVector = {offset * std::sin(angle),
-                                          offset * std::cos(angle)};
-            const Vector2 circleCenter = Vector2Add(center, offsetVector);
-            DrawCircle(circleCenter.x, circleCenter.y, 50.0f, blue);
-            const float angularVelocity = std::sin(GetTime());
+            const Vector2 armVector = {armLength * std::sin(angle),
+                                       armLength * std::cos(angle)};
+            const Vector2 circleCenter = Vector2Add(center, armVector);
+            DrawCircle(static_cast<int>(circleCenter.x),
+                       static_cast<int>(circleCenter.y), 50.0f, blue);
+            const float angularVelocity =
+                std::sin(static_cast<float>(GetTime()));
             angle += angularVelocity;
             EndDrawing();
         }
