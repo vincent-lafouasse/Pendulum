40d906907ea09cffc64044f707c1a2207a6cafb2
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-04 04:28:51 +0200

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -17,41 +17,59 @@ static constexpr float frameLen = 1.0 / targetFps;
 
 static constexpr float pendulumConstant = 1.0f;  // g/l
 
+struct Pendulum {
+    Vec2 center;
+    float armLength;
+    float theta;
+    float thetaPrime;
+
+    void render() const {
+        constexpr float armWidth = height / 150.f;
+
+        BeginDrawing();
+        const Vec2 axial = {std::sin(theta), std::cos(theta)};
+        const Vec2 transverse = {-axial.y(), axial.x()};
+        const Vec2 circleCenter = center + axial.scaled(armLength);
+
+        const Vec2 delta = transverse.scaled(armWidth);
+        const Vec2 a = center - delta;
+        const Vec2 b = center + delta;
+        const Vec2 c = circleCenter + delta;
+        const Vec2 d = circleCenter - delta;
+        DrawTriangle(a.get(), b.get(), c.get(), lavender);
+        DrawTriangle(c.get(), d.get(), a.get(), lavender);
+
+        DrawCircleV(center.get(), armWidth, lavender);
+        DrawCircleV(circleCenter.get(), 50.0f, blue);
+
+        EndDrawing();
+    }
+};
+
 int main() {
     InitWindow(width, height, "hi");
     SetTargetFPS(targetFps);
 
     constexpr Vec2 center{width / 2.0f, height / 2.0f};
     constexpr float armLength = height / 2.5f;
-    constexpr float armWidth = height / 150.f;
-    float angle = PI / 4.0f;
-    float angularSpeed = 0.0f;
+    constexpr float initialAngle = PI / 4.0f;
+    constexpr float inititialAngularSpeed = 0.0f;
+
+    Pendulum p = {
+        .center = center,
+        .armLength = armLength,
+        .theta = initialAngle,
+        .thetaPrime = inititialAngularSpeed,
+    };
 
     while (!WindowShouldClose()) {
         ClearBackground(darkGray);
-        {
-            BeginDrawing();
-            const Vec2 axial = {std::sin(angle), std::cos(angle)};
-            const Vec2 transverse = {-axial.y(), axial.x()};
-            const Vec2 circleCenter = center + axial.scaled(armLength);
 
-            const Vec2 delta = transverse.scaled(armWidth);
-            const Vec2 a = center - delta;
-            const Vec2 b = center + delta;
-            const Vec2 c = circleCenter + delta;
-            const Vec2 d = circleCenter - delta;
-            DrawTriangle(a.get(), b.get(), c.get(), lavender);
-            DrawTriangle(c.get(), d.get(), a.get(), lavender);
+        p.render();
 
-            DrawCircleV(center.get(), armWidth, lavender);
-            DrawCircleV(circleCenter.get(), 50.0f, blue);
-
-            EndDrawing();
-        }
-
-        const float angularAcceleration = -pendulumConstant * std::sin(angle);
-        angularSpeed += angularAcceleration * frameLen;
-        angle += angularSpeed * frameLen;
+        const float angularAcceleration = -pendulumConstant * std::sin(p.theta);
+        p.thetaPrime += angularAcceleration * frameLen;
+        p.theta += p.thetaPrime * frameLen;
     }
 
     CloseWindow();
