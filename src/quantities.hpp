ed2fe8f3547690b4df7b77a4a1f23806d008642b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-04 05:53:48 +0200

    make things constexpr

--- src/quantities.hpp
+++ src/quantities.hpp
@@ -3,22 +3,22 @@
 struct Mass {
     float grams;
 
-    [[nodiscard]] Mass from_kilos(float k) { return {1000.0f * k}; }
-    [[nodiscard]] Mass from_grams(float g) { return {g}; }
+    [[nodiscard]] constexpr Mass from_kilos(float k) { return {1000.0f * k}; }
+    [[nodiscard]] constexpr Mass from_grams(float g) { return {g}; }
 
-    [[nodiscard]] float as_kilos() const { return grams / 1000.0f; }
-    [[nodiscard]] float as_grams() const { return grams; }
-    [[nodiscard]] float si() const { return as_kilos(); }
+    [[nodiscard]] constexpr float as_kilos() const { return grams / 1000.0f; }
+    [[nodiscard]] constexpr float as_grams() const { return grams; }
+    [[nodiscard]] constexpr float si() const { return as_kilos(); }
 };
 
 
 struct Length {
     float millis;
 
-    [[nodiscard]] Length from_meters(float m) { return {1000.0f * m}; }
-    [[nodiscard]] Length from_millis(float m) { return {m}; }
+    [[nodiscard]] constexpr Length from_meters(float m) { return {1000.0f * m}; }
+    [[nodiscard]] constexpr Length from_millis(float m) { return {m}; }
 
-    [[nodiscard]] float as_meters() const { return millis / 1000.0f; }
-    [[nodiscard]] float as_millis() const { return millis; }
-    [[nodiscard]] float si() const { return as_meters(); }
+    [[nodiscard]] constexpr float as_meters() const { return millis / 1000.0f; }
+    [[nodiscard]] constexpr float as_millis() const { return millis; }
+    [[nodiscard]] constexpr float si() const { return as_meters(); }
 };
