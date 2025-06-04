4f4d42a6d0e83ac8f279cf91397252c4a7329b89
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-04 23:31:27 +0200

    add a 1 suffix to variables representing the first pendulum

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@ static constexpr int height = 900;
 static constexpr int targetFps = 60;
 static constexpr float frameLen = 1.0 / targetFps;
 
+static constexpr float earthAcceleration = 9.8f; // m.s-2
+
 static constexpr float scalingFactor = 100.0f;  // pixels per cm
 constexpr float pixels(Length l) {
     return scalingFactor * l.as_millis() / 10.0f;
@@ -23,8 +25,8 @@ constexpr float degreesToRadians(float deg) {
 }
 
 struct Config {
-    Length length;
-    float initialThetaDeg;
+    Length length1;
+    float initialThetaDeg1;
     float constant;
 };
 
@@ -43,18 +45,18 @@ struct Pendulum {
     Pendulum(Config cfg, LookAndFeel look)
         : cfg(cfg),
           look(look),
-          theta(degreesToRadians(cfg.initialThetaDeg)),
-          thetaPrime(0),
-          ball{0, 0} {
-        const Vec2 axial = {std::sin(theta), std::cos(theta)};
-        ball = look.center + axial.scaled(pixels(cfg.length));
+          theta1(degreesToRadians(cfg.initialThetaDeg1)),
+          thetaPrime1(0),
+          ball1{0, 0} {
+        const Vec2 axial = {std::sin(theta1), std::cos(theta1)};
+        ball1 = look.center + axial.scaled(pixels(cfg.length1));
     }
 
     const Config cfg;
     const LookAndFeel look;
-    float theta;
-    float thetaPrime;
-    Vec2 ball;
+    float theta1;
+    float thetaPrime1;
+    Vec2 ball1;
 
     void renderArm(Vec2 from, Vec2 to, Color color) const {
         const Vec2 delta =
@@ -70,19 +72,20 @@ struct Pendulum {
     void render() const {
         ClearBackground(look.backgroundColor);
         BeginDrawing();
-        this->renderArm(look.center, ball, look.armColor);
+        this->renderArm(look.center, ball1, look.armColor);
         DrawCircleV(look.center.get(), look.armWidth, look.centerColor);
-        DrawCircleV(ball.get(), look.radius, look.ballColor);
+        DrawCircleV(ball1.get(), look.radius, look.ballColor);
 
         EndDrawing();
     }
 
     void update() {
-        const float angularAcceleration = -cfg.constant * std::sin(theta);
-        thetaPrime += angularAcceleration * frameLen;
-        theta += thetaPrime * frameLen;
-        const Vec2 axial = {std::sin(theta), std::cos(theta)};
-        ball = look.center + axial.scaled(pixels(cfg.length));
+        const float angularAcceleration = -cfg.constant * std::sin(theta1);
+        thetaPrime1 += angularAcceleration * frameLen;
+        theta1 += thetaPrime1 * frameLen;
+
+        const Vec2 axial = {std::sin(theta1), std::cos(theta1)};
+        ball1 = look.center + axial.scaled(pixels(cfg.length1));
     }
 };
 
@@ -91,8 +94,8 @@ int main() {
     SetTargetFPS(targetFps);
 
     constexpr Config cfg = {
-        .length = Length::from_millis(35.0f),
-        .initialThetaDeg = 45.0f,
+        .length1 = Length::from_millis(35.0f),
+        .initialThetaDeg1 = 45.0f,
         .constant = 5.0f,
     };
 
